0a39a3f8b9e4c10c0cf1d79e1a79907861b3a36f
GALANEKO <arturo.galanm@gmail.com>
2026-08-13 17:53:02 -0600

    Update MainActivity.kt

--- app/src/main/java/com/censo/nft10/MainActivity.kt
+++ app/src/main/java/com/censo/nft10/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.censo.nft10
-import android.content.*
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import android.webkit.*
-import androidx.appcompat.app.AppCompatActivity
-class MainActivity : AppCompatActivity() {
-    lateinit var webView: WebView
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+    private var scanReceiver: BroadcastReceiver? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         webView = WebView(this)
@@ -12,16 +23,23 @@ class MainActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
         webView.loadUrl("file:///android_asset/index.html")
-        val filter = IntentFilter()
-        filter.addAction("nlscan.action.SCANNER_RESULT")
-        registerReceiver(object: BroadcastReceiver(){
-            override fun onReceive(c: Context?, i: Intent?) {
-                val code = i?.getStringExtra("SCAN_BARCODE1") ?: i?.getStringExtra("SCAN_BARCODE") ?: ""
-                if(code.isNotEmpty()){
-                    webView.evaluateJavascript("window.onScannerResult && window.onScannerResult('$code')", null)
-                }
+
+        scanReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val data = intent?.getStringExtra("SCAN_BARCODE1") ?: intent?.getStringExtra("data") ?: return
+                webView.evaluateJavascript("if(window.onScanResult){onScanResult('$data')}"){}
             }
-        }, filter)
+        }
+        registerReceiver(scanReceiver, IntentFilter("nlscan.action.SCANNER_RESULT"))
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        try { scanReceiver?.let { unregisterReceiver(it) } } catch (_: Exception) {}
     }
 }
